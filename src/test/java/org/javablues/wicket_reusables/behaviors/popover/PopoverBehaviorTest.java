3ecd63f8413c52eb8dfb2ed93eb72fcd560e969d
slawtul <slawtul@javablues.org>
2019-11-09 23:47:20 +0100

    Use tagTester for one test

--- src/test/java/org/javablues/wicket_reusables/behaviors/popover/PopoverBehaviorTest.java
+++ src/test/java/org/javablues/wicket_reusables/behaviors/popover/PopoverBehaviorTest.java
@@ -3,8 +3,12 @@ package org.javablues.wicket_reusables.behaviors.popover;
 import org.apache.wicket.core.util.string.ComponentRenderer;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.tester.TagTester;
 import org.javablues.wicket_reusables.WicketBaseTest;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,17 +24,20 @@ public class PopoverBehaviorTest extends WicketBaseTest {
         tester.startComponentInPage(panel);
 
         String actualMarkup = ComponentRenderer.renderComponent(panel).toString();
-        String expectedMarkup = new StringBuilder()
-                .append("<span wicket:id=\"panelWithButton\">")
-                .append("<wicket:panel>")
-                .append("<input type=\"submit\" wicket:id=\"register\" name=\"panelWithButton:register\" id=\"register1\" ")
-                .append("data-toggle=\"popover\" title=\"Register user\" data-content=\"Please click to register user\" ")
-                .append("data-trigger=\"hover\" data-placement=\"right\"/>")
-                .append("</wicket:panel>")
-                .append("</span>")
-                .toString();
 
-        assertEquals("Rendered panel markup does not contain correct popover markup", expectedMarkup, actualMarkup);
+        List<TagTester> dataToggle = TagTester.createTagsByAttribute(actualMarkup, "data-toggle", "popover", false);
+        List<TagTester> title = TagTester.createTagsByAttribute(actualMarkup, "title", "Register user", false);
+        List<TagTester> dataContent = TagTester.createTagsByAttribute(actualMarkup, "data-content", "Please click to register user", false);
+        List<TagTester> dataTrigger = TagTester.createTagsByAttribute(actualMarkup, "data-trigger", "hover", false);
+        List<TagTester> dataPlacement = TagTester.createTagsByAttribute(actualMarkup, "data-placement", "right", false);
+
+        Assert.assertEquals(1, dataToggle.size());
+        Assert.assertEquals(1, title.size());
+        Assert.assertEquals(1, dataContent.size());
+        Assert.assertEquals(1, dataTrigger.size());
+        Assert.assertEquals(1, dataPlacement.size());
+
+        Assert.assertEquals("input", title.get(0).getName());
     }
 
     @Test
